c7e5c194131016783470c4b5897d80c52101d18f
G-Just <gjustas00@gmail.com>
2024-01-05 05:32:59 +0200

    Tailwind styling

--- public/index.php
+++ public/index.php
@@ -4,6 +4,32 @@ require '../views/navbar.php';
 ?>
 
 
+
+<div class="text-center m-8 bg-slate-800 w-full rounded-lg shadow-lg h-min p-8">
+    <h1 class="font-bold text-2xl">Accounts</h1>
+    <table>
+        <thead class="font-bold text-gray-500">
+            <td>Owner</td>
+            <td>Personal code</td>
+            <td>Account number</td>
+            <td>Balance</td>
+            <td>Actions</td>
+        </thead>
+        <tbody>
+            <td>MORGAN DEXTER</td>
+            <td>18205101224</td>
+            <td>LT009999927248320436</td>
+            <td>$6,500.58</td>
+        </tbody>
+        <tbody>
+            <td>MORGAN DEXTER</td>
+            <td>18205101224</td>
+            <td>LT009999927248320436</td>
+            <td>$6,500.58</td>
+        </tbody>
+    </table>
+</div>
 </body>
+
 
 </html>
---
+++ public/newAccount.php
@@ -0,0 +1,8 @@
+<?php
+require '../views/head.php';
+require '../views/navbar.php';
+?>
+
+</body>
+
+</html>
