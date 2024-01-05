95eb8f345ea94c324a13e1e4e6069c728e283ea9
G-Just <gjustas00@gmail.com>
2024-01-05 23:00:23 +0200

    General development update

--- public/index.php
+++ public/index.php
@@ -6,28 +6,46 @@ $data = new App\Classes\AccountDataBaseHandler\AccountDataBaseHandler();
 $users = $data->showAll();
 ?>
 
-<div class="text-center m-8 bg-slate-800 w-1/2 rounded-lg shadow-lg h-min max-h-96 p-8 pt-4 overflow-y-auto no-scrollbar">
-    <h1 class="font-bold text-2xl">Accounts</h1>
-    <table class="w-full">
-        <thead class="font-bold text-gray-500">
-            <td>Owner</td>
-            <td>Personal code</td>
-            <td>Account number</td>
-            <td>Balance</td>
-            <td>Actions</td>
-        </thead>
-        <tbody>
-            <?php
-            foreach ($users as $user) : ?>
-                <tr>
-                    <td><?= $user['lastName'] . ' ' . $user['name'] ?></td>
-                    <td><?= $user['personalCode'] ?></td>
-                    <td><?= $user['number'] ?></td>
-                    <td><?= '$' . number_format($user['balance'], 2) ?></td>
-                </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
+<div class="w-full flex flex-col gap-4">
+    <div class="text-center m-4 mb-0 bg-slate-800 rounded-lg shadow-lg h-min max-h-96 p-8 pt-4 overflow-y-auto no-scrollbar">
+        <h1 class="font-bold text-2xl">Accounts</h1>
+        <table class="w-full border-separate border-spacing-4">
+            <thead class="font-bold text-gray-500">
+                <td>Owner</td>
+                <td>Personal code</td>
+                <td>Account number</td>
+                <td>Balance</td>
+                <td>Actions</td>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($users as $user) : ?>
+                    <tr class="font-bold tracking-wider">
+                        <td><?= $user['lastName'] . ' ' . $user['name'] ?></td>
+                        <td><?= $user['personalCode'] ?></td>
+                        <td><?= $user['number'] ?></td>
+                        <td><?= '$' . number_format($user['balance'], 2) ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="text-center w-2/5 m-4 mt-0 bg-slate-800 rounded-lg shadow-lg h-min max-h-96 p-8 pt-4 overflow-y-auto no-scrollbar">
+        <h1 class="font-bold text-2xl">Log</h1>
+        <div class="flex flex-col">
+            <div class="flex content-center">
+                <img class="w-12 h-w-12 rounded-full" src="./img/pfp.jpg" alt="user pfp">
+                <div>
+                    <p>
+                        <span class="font-bold">User Userer</span>
+                        created account
+                        <span class="font-bold">LT2374284792</span>
+                        <br><span>Just Now</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 </body>
--- views/navbar.php
+++ views/navbar.php
@@ -1,4 +1,4 @@
-<body class="bg-slate-950 h-screen m-0 text-white flex">
+<body class="bg-slate-950 h-screen m-0 text-white flex overflow-hidden">
 
     <div class="p-4 min-w-80 h-screen bg-slate-900 border-r-2 border-neutral-700 flex flex-col items-center">
         <div class="text-center">
@@ -6,8 +6,8 @@
             <h1 class="text-2xl font-extrabold tracking-widest">PHP Bank</h1>
             <h3 class="text-teal-600 font-semibold leading-none">Financial institution</h1>
         </div>
-        <div class="mt-14 flex flex-col items-center gap-6">
-            <a class="text-2xl underline underline-offset-8 hover:text-slate-500" href="./">Accounts</a>
-            <a class="text-2xl underline underline-offset-8 hover:text-slate-500" href="./newAccount.php">Create account</a>
+        <div class="mt-14 flex flex-col items-start gap-6">
+            <a class="flex gap-4 text-2xl hover:text-slate-500" href="./"><img class="w-7 h-w-7" src="../public/img/home.svg" alt="">Home</a>
+            <a class="flex gap-4 text-2xl hover:text-slate-500" href="./newAccount.php"><img class="w-7 h-w-7" src="../public/img/create.svg" alt="">Create account</a>
         </div>
     </div>
