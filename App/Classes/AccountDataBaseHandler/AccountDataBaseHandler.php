8cce6870eeb1929234b9634d9a9c597c8372528a
G-Just <gjustas00@gmail.com>
2024-01-07 05:18:26 +0200

    General development update

--- App/Classes/AccountDataBaseHandler/AccountDataBaseHandler.php
+++ App/Classes/AccountDataBaseHandler/AccountDataBaseHandler.php
@@ -4,67 +4,73 @@ namespace App\Classes\AccountDataBaseHandler;
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
-
 use App\Classes\_DB\DataBase;
-
 
 class AccountDataBaseHandler implements DataBase
 {
-    private $data;
+    private $accounts;
     private $log;
     private function refreshData(): void
     {
-        $this->data = json_decode(file_get_contents(__DIR__ . '/../../database/data.JSON'), true);
+        $this->accounts = json_decode(file_get_contents(__DIR__ . '/../../database/data.JSON'), true);
         $this->log = json_decode(file_get_contents(__DIR__ . '/../../database/log.JSON'), true);
     }
-    private function writeData(array $data, array $log): void
+    private function writeData(array $accounts, array $log): void
     {
-        file_put_contents(__DIR__ . '/../../database/data.JSON', json_encode($data, JSON_PRETTY_PRINT));
-        file_put_contents(__DIR__ . '/../../database/log.JSON', json_encode('', JSON_PRETTY_PRINT));
+        file_put_contents(__DIR__ . '/../../database/data.JSON', json_encode($accounts, JSON_PRETTY_PRINT));
+        file_put_contents(__DIR__ . '/../../database/log.JSON', json_encode($log, JSON_PRETTY_PRINT));
     }
     function create(array $userData): void
     {
         $this->refreshData();
-        if (isset((end($this->data)['id']))) {
-            $id = (end($this->data)['id']) + 1;
+        if (isset((end($this->accounts)['id']))) {
+            $id = (end($this->accounts)['id']) + 1;
         } else {
             $id = 0;
         }
-        array_push($this->data, ['id' => $id] + $userData);
-        $this->writeData($this->data, $this->log);
+        array_push($this->accounts, ['id' => $id] + $userData);
+        // array_push($this->log, ['id' => $_SESSION['id'], 'action' => 'created account', 'accountID' => $id, 'time' => date('F/d')]);
+        date_default_timezone_set("Europe/Vilnius");
+        array_push($this->log, ['id' => 0, 'action' => 'created account', 'accountID' => $id, 'time' => date('Y F, d @ H:i'), 'amount' => 0]);
+        $this->writeData($this->accounts, $this->log);
     }
     function update(int $userId, array $userData): void
     {
         $this->refreshData();
-        foreach ($this->data as $user) {
+        foreach ($this->accounts as $user) {
             if ($userId === $user['id']) {
                 $user = $userData;
             }
         }
-        $this->writeData($this->data, $this->log);
+        $this->writeData($this->accounts, $this->log);
     }
     function delete(int $userId): void
     {
         $this->refreshData();
-        foreach ($this->data as $key => $user) {
+        foreach ($this->accounts as $key => $user) {
             if ($userId === $user['id']) {
-                unset($this->data[$key]);
+                unset($this->accounts[$key]);
             }
         }
-        $this->writeData($this->data, $this->log);
+        $this->writeData($this->accounts, $this->log);
     }
     function show(int $userId): array
     {
         $this->refreshData();
-        foreach ($this->data as $user) {
-            if ($user['id'] === $userId) {
-                return $user;
+        foreach ($this->accounts as $account) {
+            if ($account['id'] === $userId) {
+                return $account;
             }
         }
     }
     function showAll(): array
     {
         $this->refreshData();
-        return $this->data;
+        return $this->accounts;
+    }
+    function LogShowAll(): array
+    {
+        $this->refreshData();
+        return $this->log;
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,14 +2,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 require '../views/head.php';
 require '../views/navbar.php';
-$data = new App\Classes\AccountDataBaseHandler\AccountDataBaseHandler();
-$users = $data->showAll();
+$AccountData = new App\Classes\AccountDataBaseHandler\AccountDataBaseHandler();
+$UserData = new App\Classes\UserDataBaseHandler\UserDataBaseHandler();
+$accounts = $AccountData->showAll();
+$users = $UserData->showAll();
+$log = $AccountData->LogShowAll();
+// $AccountData->create(['name' => "Test", 'lastName' => "Tester", 'number' => 'LT009999995641657700', 'personalCode' => '22723842328', 'balance' => 0]);
 ?>
 
 <div class="w-full flex flex-col gap-4">
     <div class="text-center m-4 mb-0 bg-slate-800 rounded-lg shadow-lg min-h-96 p-8 pt-4">
         <h1 class="font-bold text-2xl">Accounts</h1>
-        <div class="overflow-y-auto h-full no-scrollbar">
+        <div class="overflow-y-auto h-full">
             <table class="w-full border-separate border-spacing-4 text-left">
                 <thead class="text-xl font-bold text-gray-500 sticky top-0 bg-slate-800">
                     <th>Owner</th>
@@ -20,43 +24,64 @@ $users = $data->showAll();
                 </thead>
                 <tbody>
                     <?php
-                    foreach ($users as $user) : ?>
+                    foreach ($accounts as $account) : ?>
                         <tr class="font-bold tracking-wider w-full">
-                            <td><?= $user['lastName'] . ' ' . $user['name'] ?></td>
-                            <td><?= $user['personalCode'] ?></td>
-                            <td><?= $user['number'] ?></td>
-                            <td><?= '$' . number_format($user['balance'], 2) ?></td>
+                            <td><?= $account['lastName'] . ' ' . $account['name'] ?></td>
+                            <td><?= $account['personalCode'] ?></td>
+                            <td><?= $account['number'] ?></td>
+                            <td><?= '$' . number_format($account['balance'], 2) ?></td>
                         </tr>
                     <?php endforeach ?>
                 </tbody>
             </table>
         </div>
     </div>
-    <div class="flex flex-col gap-8 text-center w-2/5 m-4 mt-0 bg-slate-800 rounded-lg shadow-lg min-h-80 p-8 pt-4 overflow-hidden">
-        <h1 class="font-bold text-2xl sticky">Log</h1>
-        <div class="flex flex-col-reverse gap-12 overflow-y-auto no-scrollbar">
-            <div class="flex gap-6 relative after:content-[''] after:absolute after:top-14 after:-bottom-10 after:left-6 after:w-px after:bg-gray-500">
-                <img class="w-12 h-w-12 rounded-full" src="./img/pfp.jpg" alt="user pfp">
-                <div>
-                    <p>
-                        <span class="font-bold">John Smith</span>
-                        created account
-                        <span class="font-bold">LT009999995641657718</span>
-                    </p>
-                    <p class="text-left text-sm text-gray-500">Just Now</p>
-                </div>
+    <div class="flex min-h-80 m-4 mt-0 gap-4">
+        <div class="flex flex-col gap-8 text-center w-2/4 bg-slate-800 rounded-lg shadow-lg min-h-80 p-8 pt-4 overflow-hidden">
+            <h1 class="font-bold text-2xl sticky">Log</h1>
+            <div class="flex flex-col-reverse gap-12 overflow-y-auto">
+                <?php foreach ($log as $entry) : ?>
+                    <div class="flex gap-6 relative after:content-[''] after:absolute after:top-14 after:-bottom-10 after:left-6 after:w-px after:bg-gray-500">
+                        <img class="w-12 h-12 rounded-full" src="./img/pfp.jpg" alt="user pfp">
+                        <div>
+                            <p class="text-left">
+                                <span class="font-bold">
+                                    <?php foreach ($users as $user) {
+                                        if ($entry['id'] === $user['id']) {
+                                            echo $user['username'];
+                                            break;
+                                        }
+                                    } ?>
+                                </span>
+                                <?php
+                                if ($entry['action'] === 'deposited' || $entry['action'] === 'withdrew') {
+                                    echo $entry['action'] . " $<span class='font-bold'></span><span class='font-bold'>" . $entry['amount'] . '</span>';
+                                    echo $entry['action'] === "deposited" ? ' into' : ' out of';
+                                    echo " account ";
+                                } else {
+                                    echo $entry['action'] . ' account';
+                                }
+                                ?>
+                                <span class="font-bold">
+                                    <?php foreach ($accounts as $account) {
+                                        if ($entry['accountID'] === $account['id']) {
+                                            echo $account['number'] . "<span class='font-normal'> for</span> " . $account['name'] . ' ' . $account['lastName'];
+                                            break;
+                                        }
+                                    } ?>
+                                </span>
+                            </p>
+                            <p class="text-left text-sm text-gray-500"><?= $entry['time'] ?></p>
+                        </div>
+                    </div>
+                <?php endforeach ?>
             </div>
-            <div class="flex gap-6 relative after:content-[''] after:absolute after:top-14 after:-bottom-10 after:left-6 after:w-px after:bg-gray-500">
-                <img class="w-12 h-w-12 rounded-full" src="./img/pfp.jpg" alt="user pfp">
-                <div>
-                    <p>
-                        <span class="font-bold">John Doe</span>
-                        created account
-                        <span class="font-bold">LT009999995641657718</span>
-                    </p>
-                    <p class="text-left text-sm text-gray-500">Just Now</p>
-                </div>
-            </div>
+        </div>
+        <div class="grid gap-8 text-center w-full bg-slate-800 rounded-lg shadow-lg p-8 pt-4 overflow-hidden">
+            <h1 class="font-bold text-2xl sticky">Statistics</h1>
+            <div>Open accounts</div>
+            <div>Registered Users</div>
+            <div>Total holdings</div>
         </div>
     </div>
 </div>
