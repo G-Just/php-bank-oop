d9b760f9ac89351c31e50d0a88ce878842bd007d
G-Just <gjustas00@gmail.com>
2024-01-18 12:41:39 +0200

    Styling update and adding chars to account page

--- views/account.php
+++ views/account.php
@@ -1,70 +1,131 @@
-<body>
-    <div class="w-full h-full bg-[url('/../img/bg_pattern.jpg')] bg-cover">
-        <div class="flex justify-between p-10 m-4 mb-0 rounded-lg shadow-lg bg-slate-800">
-            <div class="flex flex-col gap-3">
-                <h1 class="text-5xl"><?= $data['account']['name'] . ' ' . $data['account']['lastName'] ?></h1>
-                <p class="tracking-widest text-teal-600"><?= $data['account']['personalCode'] ?></p>
-            </div>
-            <div>
-                <h1>
-                    <p class="text-4xl tracking-widest"><?= $data['account']['number'] ?></p>
-                    <p>&nbsp</p>
-                </h1>
-            </div>
-        </div>
-        <div class="grid grid-cols-2 gap-4 m-4">
-            <div class="flex flex-col items-center gap-4 p-8 pt-4 rounded-lg shadow-lg bg-slate-800">
-                <h1 class="text-5xl">Balance</h1>
-                <hr class="w-60 border-px">
-                <h1 class="text-8xl"><?= '$' . number_format($data['account']['balance'], 2) ?></h1>
-                <div class="grid w-full grid-cols-2 gap-4 m-8 mb-2 text-center">
-                    <a class="px-5 py-3 text-xl font-bold border-2 rounded-lg border-neutral-700 bg-emerald-800 bg-opacity-20 hover:bg-opacity-90" href=<?= URL . 'account/deposit/' . $data['account']['id'] ?>>Deposit</a>
-                    <a class="px-5 py-3 text-xl font-bold bg-yellow-900 border-2 rounded-lg border-neutral-700 bg-opacity-20 hover:bg-opacity-90" href=<?= URL . 'account/withdraw/' . $data['account']['id'] ?>>Withdraw</a>
-                    <a class="col-span-2 px-5 py-3 text-xl font-bold bg-red-800 border-2 rounded-lg border-neutral-700 bg-opacity-20 hover:bg-opacity-90" href=<?= URL . 'account/delete/' . $data['account']['id'] ?>>Delete</a>
-                    <p class="col-span-2 text-red-600"><?= $_SESSION['error'] ?? '' ?></p>
-                    <p class="col-span-2 text-green-600"><?= $_SESSION['message'] ?? '' ?></p>
+        <script src="path/to/chartjs/dist/chart.umd.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" integrity="sha512-ElRFoEQdI5Ht6kZvyzXhYG9NqjtkmlkfYk0wr6wHxU9JEHakS7UJZNeml5ALk+8IKlU6jDgMabC3vkumRokgJA==" crossorigin="anonymous" referrerpolicy="no-referrer">
+        </script>
+
+        <body>
+            <div class="w-full h-full grid grid-cols-2 grid-rows-10 p-4 gap-4 bg-[url('/../img/bg_pattern.jpg')] bg-cover">
+                <div class="flex justify-between col-span-2 row-span-2 p-10 rounded-lg shadow-lg bg-slate-800">
+                    <div class="flex flex-col gap-3">
+                        <h1 class="text-5xl"><?= $data['account']['name'] . ' ' . $data['account']['lastName'] ?></h1>
+                        <p class="tracking-widest text-teal-600"><?= $data['account']['personalCode'] ?></p>
+                    </div>
+                    <div>
+                        <h1>
+                            <p class="text-4xl tracking-widest"><?= $data['account']['number'] ?></p>
+                            <p>&nbsp</p>
+                        </h1>
+                    </div>
+                </div>
+                <div class="flex flex-col items-center col-span-1 row-span-5 gap-4 p-8 pt-4 rounded-lg shadow-lg bg-slate-800">
+                    <h1 class="text-5xl">Balance</h1>
+                    <hr class="w-60 border-px">
+                    <h1 class="text-8xl"><?= '$' . number_format($data['account']['balance'], 2) ?></h1>
+                    <div class="grid w-full grid-cols-2 gap-4 m-8 mb-px text-center">
+                        <a class="px-5 py-3 text-xl font-bold border-2 rounded-lg border-neutral-700 bg-emerald-800 bg-opacity-20 hover:bg-opacity-90" href=<?= URL . 'account/deposit/' . $data['account']['id'] ?>>Deposit</a>
+                        <a class="px-5 py-3 text-xl font-bold bg-yellow-900 border-2 rounded-lg border-neutral-700 bg-opacity-20 hover:bg-opacity-90" href=<?= URL . 'account/withdraw/' . $data['account']['id'] ?>>Withdraw</a>
+                        <a class="col-span-2 px-5 py-3 text-xl font-bold bg-red-800 border-2 rounded-lg border-neutral-700 bg-opacity-20 hover:bg-opacity-90" href=<?= URL . 'account/delete/' . $data['account']['id'] ?>>Delete</a>
+                    </div>
+                    <div class="flex">
+                        <p class="col-span-2 text-red-600"><?= $_SESSION['error'] ?? '' ?></p>
+                        <p class="col-span-2 text-green-600"><?= $_SESSION['message'] ?? '' ?></p>
+                    </div>
+                </div>
+                <div class="grid-cols-1 col-span-1 row-span-5 p-10 rounded-lg shadow-lg bg-slate-800">
+                    <canvas id="chart"></canvas>
+                    <script>
+                        function drawChart() {
+                            const labels = ['Investments', 'Food', 'Transportation', 'Misc'];
+                            const data = {
+                                labels: labels,
+                                datasets: [{
+                                    label: 'Budget',
+                                    data: [Math.random() * 3000, Math.random() * 500, Math.random() * 500, Math.random() * 2000],
+                                    backgroundColor: [
+                                        'rgba(63, 81, 181, 0.5)',
+                                        'rgba(77, 182, 172, 0.5)',
+                                        'rgba(66, 133, 244, 0.5)',
+                                        'rgba(156, 39, 176, 0.5)',
+                                        'rgba(233, 30, 99, 0.5)',
+                                        'rgba(66, 73, 244, 0.4)',
+                                        'rgba(66, 133, 244, 0.2)',
+                                    ],
+                                }, ],
+                            };
+                            const configChart = {
+                                type: "doughnut",
+                                data,
+                                options: {
+                                    responsive: true,
+                                    maintainAspectRatio: false,
+                                    borderWidth: 1,
+                                    hoverOffset: 20
+                                }
+                            };
+                            var chartLine = new Chart(
+                                document.getElementById("chart"),
+                                configChart
+                            );
+                        }
+                        drawChart();
+                    </script>
+                </div>
+                <div class="col-span-2 row-span-3 rounded-lg shadow-lg bg-slate-800">
+
+                    <canvas class="w-full h-full p-2" id="chartLine"></canvas>
+                    <!-- Required chart.js -->
+                    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                    <!-- Chart line -->
+                    <script>
+                        function* splitNParts(num, parts, min) {
+                            let remParts = parts;
+                            let sumParts = num;
+                            for (let i = 0; i < parts - 1; i++) {
+                                const pn = Math.ceil(Math.random() * (sumParts - (remParts * min)) + min)
+                                yield pn
+                                sumParts -= pn;
+                                remParts -= 1;
+                            }
+                            yield sumParts;
+                        }
+                        const labels = ["2019", "2020", "2021", "2022", "2023", "2024"];
+                        const values = [...splitNParts(<?= $data['account']['balance'] ?>, 5, 0)].sort((a, b) => a - b)
+                        const data = {
+                            labels: labels,
+                            datasets: [{
+                                label: "Balance",
+                                backgroundColor: "hsl(222, 47%, 11%)",
+                                borderColor: "hsl(0, 0%, 100%)",
+                                borderWidth: 1,
+                                data: [...values, <?= $data['account']['balance'] ?>],
+                                fill: true
+                            }, ],
+                        };
+                        const configLineChart = {
+                            type: "line",
+                            data,
+                            options: {
+                                responsive: true,
+                                maintainAspectRatio: false,
+                                scales: {
+                                    y: {
+                                        ticks: {
+                                            callback: function(value, index, ticks) {
+                                                return '$' + value;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        };
+                        var chartLine = new Chart(
+                            document.getElementById("chartLine"),
+                            configLineChart
+                        );
+                    </script>
+
+
                 </div>
             </div>
-            <div class="h-full p-10 rounded-lg shadow-lg bg-slate-800">
-                <canvas id="chart"></canvas>
-                <script src="path/to/chartjs/dist/chart.umd.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" integrity="sha512-ElRFoEQdI5Ht6kZvyzXhYG9NqjtkmlkfYk0wr6wHxU9JEHakS7UJZNeml5ALk+8IKlU6jDgMabC3vkumRokgJA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-                <script>
-                    const labels = ['Investments', 'Food', 'Transportation', 'Misc'];
-                    const data = {
-                        labels: labels,
-                        datasets: [{
-                            label: 'Budget',
-                            data: [Math.random() * 3000, Math.random() * 500, Math.random() * 500, Math.random() * 2000],
-                            backgroundColor: [
-                                'rgba(63, 81, 181, 0.5)',
-                                'rgba(77, 182, 172, 0.5)',
-                                'rgba(66, 133, 244, 0.5)',
-                                'rgba(156, 39, 176, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(66, 73, 244, 0.4)',
-                                'rgba(66, 133, 244, 0.2)',
-                            ],
-                        }, ],
-                    };
-                    const configChart = {
-                        type: "doughnut",
-                        data,
-                        options: {
-                            responsive: true,
-                            maintainAspectRatio: false,
-                            borderWidth: 1,
-                            hoverOffset: 20
-                        }
-                    };
-                    var chartLine = new Chart(
-                        document.getElementById("chart"),
-                        configChart
-                    );
-                </script>
-            </div>
-        </div>
-    </div>
-</body>
+        </body>
 
-</html>
+        </html>
--- views/home.php
+++ views/home.php
@@ -1,116 +1,118 @@
 <body>
-    <div class="flex flex-col w-full gap-4 bg-[url('/../img/bg_pattern.jpg')] bg-cover">
-        <div class="pt-4 pb-8 m-4 mb-0 text-center rounded-lg shadow-lg min-h-96 bg-slate-800">
+    <div class="p-4 grid grid-cols-3 grid-rows-2 w-full gap-4 bg-[url('/../img/bg_pattern.jpg')] bg-cover">
+        <div class="relative col-span-3 pt-4 pb-8 text-center rounded-lg shadow-lg bg-slate-800">
             <h1 class="text-2xl font-bold">Accounts</h1>
             <div class="h-full overflow-y-auto">
-                <table class="min-w-full mb-2 leading-normal">
-                    <thead>
-                        <tr class="sticky">
-                            <th class="sticky top-0 px-5 py-3 text-sm font-semibold tracking-wider text-left text-gray-500 uppercase border-b-2 border-neutral-600 bg-slate-800">
-                                Owner
-                            </th>
-                            <th class="sticky top-0 px-5 py-3 text-sm font-semibold tracking-wider text-left text-gray-500 uppercase border-b-2 border-neutral-600 bg-slate-800">
-                                Personal Code
-                            </th>
-                            <th class="sticky top-0 px-5 py-3 text-sm font-semibold tracking-wider text-left text-gray-500 uppercase border-b-2 border-neutral-600 bg-slate-800">
-                                Account Number
-                            </th>
-                            <th class="sticky top-0 px-5 py-3 text-sm font-semibold tracking-wider text-left text-gray-500 uppercase border-b-2 border-neutral-600 bg-slate-800">
-                                Balance
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($data['accounts'] as $user) : ?>
-                            <tr onclick="window.location=`/account/dashboard/<?= $user['id'] ?>`;" class="cursor-pointer hover:bg-slate-900 hover:text-teal-600">
-                                <td class="px-5 py-5 text-lg border-b border-neutral-600x">
-                                    <div class="flex items-center">
-                                        <div>
-                                            <p class=whitespace-no-wrap ">
+                <?php if (isset($_SESSION['id'])) { ?>
+                    <table class="min-w-full mb-2 leading-normal">
+                        <thead>
+                            <tr class="sticky">
+                                <th class="sticky top-0 px-5 py-3 text-sm font-semibold tracking-wider text-left text-gray-500 uppercase border-b-2 border-neutral-600 bg-slate-800">
+                                    Owner
+                                </th>
+                                <th class="sticky top-0 px-5 py-3 text-sm font-semibold tracking-wider text-left text-gray-500 uppercase border-b-2 border-neutral-600 bg-slate-800">
+                                    Personal Code
+                                </th>
+                                <th class="sticky top-0 px-5 py-3 text-sm font-semibold tracking-wider text-left text-gray-500 uppercase border-b-2 border-neutral-600 bg-slate-800">
+                                    Account Number
+                                </th>
+                                <th class="sticky top-0 px-5 py-3 text-sm font-semibold tracking-wider text-left text-gray-500 uppercase border-b-2 border-neutral-600 bg-slate-800">
+                                    Balance
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($data['accounts'] as $user) : ?>
+                                <tr onclick="window.location=`/account/dashboard/<?= $user['id'] ?>`;" class="cursor-pointer hover:bg-slate-900 hover:text-teal-600">
+                                    <td class="px-5 py-5 text-lg border-b border-neutral-600x">
+                                        <div class="flex items-center">
+                                            <div>
+                                                <p class=whitespace-no-wrap ">
                                                 <?= $user['lastName'] . ' ' . $user['name'] ?>
                                             </p>
                                         </div>
                                     </div>
                                 </td>
                                 <td class=" px-5 py-5 text-lg border-b border-neutral-600x">
-                                            <p class="text-left whitespace-no-wrap "><?= $user['personalCode'] ?></p>
-                                </td>
-                                <td class="px-5 py-5 text-lg border-b border-neutral-600x">
-                                    <p class="text-left whitespace-no-wrap ">
-                                        <?= $user['number'] ?>
-                                    </p>
-                                </td>
-                                <td class="px-5 py-5 text-lg border-b border-neutral-600x">
-                                    <p class="text-left whitespace-no-wrap ">
-                                        <?= '$' . number_format($user['balance'], 2) ?>
-                                    </p>
-                                </td>
-                            </tr>
-                        <?php endforeach ?>
-                    </tbody>
-                </table>
+                                                <p class="text-left whitespace-no-wrap "><?= $user['personalCode'] ?></p>
+                                    </td>
+                                    <td class="px-5 py-5 text-lg border-b border-neutral-600x">
+                                        <p class="text-left whitespace-no-wrap ">
+                                            <?= $user['number'] ?>
+                                        </p>
+                                    </td>
+                                    <td class="px-5 py-5 text-lg border-b border-neutral-600x">
+                                        <p class="text-left whitespace-no-wrap ">
+                                            <?= '$' . number_format($user['balance'], 2) ?>
+                                        </p>
+                                    </td>
+                                </tr>
+                            <?php endforeach ?>
+                        <?php } else { ?>
+                            <p class="absolute text-2xl -translate-x-1/2 top-1/2 left-1/2">Login to view account data</p>
+                        <?php } ?>
+                        </tbody>
+                    </table>
             </div>
         </div>
-        <div class="flex gap-4 m-4 mt-0 max-h-96 min-h-96">
-            <div class="flex flex-col w-2/4 gap-8 p-8 pt-4 overflow-hidden text-center rounded-lg shadow-lg bg-slate-800 min-h-80">
-                <h1 class="sticky text-2xl font-bold">Log</h1>
-                <div class="flex flex-col overflow-y-auto gap-11">
-                    <?php foreach ($data['log'] as $entry) : ?>
-                        <div class="flex gap-6 relative after:content-[''] after:absolute after:top-16 after:-bottom-11 after:left-7 after:w-px after:bg-gray-500">
-                            <img class="h-16 rounded-full w-h-16" src="<?= URL . 'img/' . $entry['image'] ?>" alt="action icon">
-                            <div>
-                                <p class="font-normal text-left">
-                                    <span class="font-bold text-teal-600"><?= $entry['user'] ?></span>
-                                    <?= $entry['action'] ?><br>
-                                    <span class="font-bold">
-                                        <?= $entry['account'] ?>
-                                    </span>
-                                    <span class="font-bold text-teal-600"><?= $entry['name'] ?></span>
-                                </p>
-                                <p class="text-sm text-left text-gray-500"><?= $entry['time'] ?></p>
-                            </div>
+        <div class="p-8 pt-4 overflow-hidden text-center rounded-lg shadow-lg bg-slate-800">
+            <h1 class="mb-4 text-2xl font-bold">Log</h1>
+            <div class="flex flex-col h-full pt-4 overflow-y-auto gap-11">
+                <?php foreach ($data['log'] as $entry) : ?>
+                    <div class="flex gap-6 relative after:content-[''] after:absolute after:top-16 after:-bottom-11 after:left-7 after:w-px after:bg-gray-500">
+                        <img class="h-16 rounded-full w-h-16" src="<?= URL . 'img/' . $entry['image'] ?>" alt="action icon">
+                        <div>
+                            <p class="font-normal text-left">
+                                <span class="font-bold text-teal-600"><?= $entry['user'] ?></span>
+                                <?= $entry['action'] ?><br>
+                                <span class="font-bold">
+                                    <?= $entry['account'] ?>
+                                </span>
+                                <span class="font-bold text-teal-600"><?= $entry['name'] ?></span>
+                            </p>
+                            <p class="text-sm text-left text-gray-500"><?= $entry['time'] ?></p>
                         </div>
-                    <?php endforeach ?>
-                </div>
+                    </div>
+                <?php endforeach ?>
             </div>
-            <div class="w-full gap-4 p-10 pt-4 overflow-hidden text-center rounded-lg shadow-lg bg-slate-800">
-                <h1 class="mb-2 text-2xl font-bold">Statistics</h1>
-                <div class="grid h-full grid-cols-3 gap-4 py-4">
-                    <div class="pl-2 bg-teal-800 rounded-xl bg-clip-padding">
-                        <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
-                            <p class="px-2 text-4xl font-bold"><?= $data['stats'][0]['accountCount'] ?></p>
-                            <p class="mt-2 text-sm text-teal-600">Opened bank accounts</p>
-                        </div>
+        </div>
+        <div class="col-span-2 p-8 pt-4 overflow-hidden text-center rounded-lg shadow-lg bg-slate-800">
+            <h1 class="mb-2 text-2xl font-bold">Statistics</h1>
+            <div class="grid h-full grid-cols-3 gap-4 pt-4 pb-8">
+                <div class="pl-2 bg-teal-800 rounded-xl bg-clip-padding">
+                    <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
+                        <p class="px-2 text-4xl font-bold"><?= $data['stats'][0]['accountCount'] ?></p>
+                        <p class="mt-2 text-sm text-teal-600">Opened bank accounts</p>
                     </div>
-                    <div class="pl-2 bg-emerald-800 rounded-xl bg-clip-padding">
-                        <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
-                            <p class="px-2 text-4xl font-bold"><?= $data['stats'][0]['userCount'] ?></p>
-                            <p class="mt-2 text-sm text-teal-600">Registered users</p>
-                        </div>
+                </div>
+                <div class="pl-2 bg-emerald-800 rounded-xl bg-clip-padding">
+                    <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
+                        <p class="px-2 text-4xl font-bold"><?= $data['stats'][0]['userCount'] ?></p>
+                        <p class="mt-2 text-sm text-teal-600">Registered users</p>
                     </div>
-                    <div class="pl-2 bg-sky-950 rounded-xl bg-clip-padding">
-                        <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
-                            <p class="px-2 text-3xl font-bold"><?= '$' . number_format($data['stats'][0]['totalHoldings'], 2) ?></p>
-                            <p class="mt-2 text-sm text-teal-600">Total holdings</p>
-                        </div>
+                </div>
+                <div class="pl-2 bg-sky-950 rounded-xl bg-clip-padding">
+                    <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
+                        <p class="px-2 text-3xl font-bold"><?= '$' . number_format($data['stats'][0]['totalHoldings'], 2) ?></p>
+                        <p class="mt-2 text-sm text-teal-600">Total holdings</p>
                     </div>
-                    <div class="pl-2 bg-cyan-800 rounded-xl bg-clip-padding">
-                        <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
-                            <p class="px-2 text-3xl font-bold"><?= '$' . number_format($data['stats'][0]['averageHoldings'], 2) ?></p>
-                            <p class="mt-2 text-sm text-teal-600">Average balance</p>
-                        </div>
+                </div>
+                <div class="pl-2 bg-cyan-800 rounded-xl bg-clip-padding">
+                    <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
+                        <p class="px-2 text-3xl font-bold"><?= '$' . number_format($data['stats'][0]['averageHoldings'], 2) ?></p>
+                        <p class="mt-2 text-sm text-teal-600">Average balance</p>
                     </div>
-                    <div class="pl-2 bg-lime-900 rounded-xl bg-clip-padding">
-                        <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
-                            <p class="px-2 text-3xl font-bold"><?= '$' . number_format($data['stats'][0]['minHoldings'], 2) ?></p>
-                            <p class="mt-2 text-sm text-teal-600">Min balance</p>
-                        </div>
+                </div>
+                <div class="pl-2 bg-lime-900 rounded-xl bg-clip-padding">
+                    <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
+                        <p class="px-2 text-3xl font-bold"><?= '$' . number_format($data['stats'][0]['minHoldings'], 2) ?></p>
+                        <p class="mt-2 text-sm text-teal-600">Min balance</p>
                     </div>
-                    <div class="pl-2 bg-indigo-900 rounded-xl bg-clip-padding">
-                        <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
-                            <p class="px-2 text-3xl font-bold"><?= '$' . number_format($data['stats'][0]['maxHoldings'], 2) ?></p>
-                            <p class="mt-2 text-sm text-teal-600">Max balance</p>
-                        </div>
+                </div>
+                <div class="pl-2 bg-indigo-900 rounded-xl bg-clip-padding">
+                    <div class="relative flex flex-col items-center justify-center h-full -mr-px bg-slate-900 rounded-xl">
+                        <p class="px-2 text-3xl font-bold"><?= '$' . number_format($data['stats'][0]['maxHoldings'], 2) ?></p>
+                        <p class="mt-2 text-sm text-teal-600">Max balance</p>
                     </div>
                 </div>
             </div>
