77a5817686db49d8f3b7513bf08563e907c5fbe3
G-Just <gjustas00@gmail.com>
2024-01-21 11:26:47 +0200

    Appending database class

--- App/Db/DataBaseHandler.php
+++ App/Db/DataBaseHandler.php
@@ -28,6 +28,8 @@ class DataBaseHandler implements DataBase
     }
     public function create(array $userData): void
     {
+        $sql = "INSERT INTO {$this->table} (accountID, accountName, accountLastName, accountNumber, accountPersonalCode, accountBalance) VALUES (?, ?, ?, ?, ?, ?)";
+        $this->pdo->prepare($sql)->execute([$userData['id'], $userData['fname'], $userData['lname'], $userData['num'], $userData['code'], $userData['balance']]);
     }
     public function update(int $userId, array $userData): void
     {
@@ -37,12 +39,19 @@ class DataBaseHandler implements DataBase
     }
     public function show(int $userId): array
     {
-        return [];
+        $sql = "SELECT * FROM {$this->table} WHERE accountID=?";
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute([$userId]);
+        return $statement->fetch();
     }
     public function showAll(): array
-    {
-        return [];
+    { {
+            $sql = "SELECT * FROM {$this->table}";
+            $statement = $this->pdo->query($sql);
+            return $statement->fetchAll();
+        }
     }
 }
-
 $x = new DataBaseHandler('accounts');
+// $x->create(['id' => 0, 'fname' => 'John', 'lname' => 'James', 'num' => 'LT3249i2', 'code' => '38239498', 'balance' => 0]);
+// print_r($x->show(0));
