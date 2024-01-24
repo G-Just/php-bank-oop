1201a8b363eacf186c47caa064e1d700f96c3a85
G-Just <gjustas00@gmail.com>
2024-01-24 09:48:19 +0200

    Adding database fed user list

--- App/Controllers/UserListController.php
+++ App/Controllers/UserListController.php
@@ -4,13 +4,14 @@ namespace App\Controllers;
 
 use App\Db\FileBaseHandler;
 use App\Core\Controller;
+use App\Db\DataBaseHandler;
 
 class UserListController extends Controller
 {
     public function index()
     {
         if (isset($_SESSION['id'])) {
-            $db = new FileBaseHandler('users');
+            $_SESSION['db'] === 'file' ? $db = new FileBaseHandler('users') : $db = new DataBaseHandler('users');
             $users = $db->showAll();
             return $this->view('userlist', ['users' => $users]);
         } else {
